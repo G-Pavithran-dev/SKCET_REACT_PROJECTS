3b1c2c202f83c6b46774f5fa04b14279f5980ba8
G-Pavithran-dev <pavithranadithya1441@gmail.com>
2023-11-20 12:29:29 +0530

    jsonServer class task completed

--- class_tasks/src/App.js
+++ class_tasks/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import  ParentContext  from './components/contextDemo';
+// import  ParentContext  from './components/contextDemo';
+import JsonServerDemo from './components/jsonServer';
 
 function App() {
   return (
     <>
-      <ParentContext />
+      <JsonServerDemo />
     </>
   );
 }
